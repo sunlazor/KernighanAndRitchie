2712d00220d833ba4ec10ada5f97023534059020
sunlazor <sunlazor@commit>
2013-08-03 21:59:25 +0400

    ex 5.3 done
    thx to tehten

---
+++ 5.3/ex.mk
@@ -0,0 +1,3 @@
+project : D:\Project\KernighanAndRitchie\5.3\ex.exe .SYMBOLIC
+
+!include D:\Project\KernighanAndRitchie\5.3\ex.mk1
--- 5.3/main.c
+++ 5.3/main.c
@@ -1,19 +1,45 @@
+/*
+* pg. 62
+* strcat: присоедин€ет строку t к концу s; 
+* в s должно быть достаточно места
+*/
+/*
+* Ex. 5.3 K&R pg 119
+* ”пражнение 5.3.
+* Ќапишите свою версию функции streat,
+* продемонстрированной в главе 2,
+* с применением указателей. Ќапоминаем,
+* что strcat (s, t) копирует строку t
+* в конец строки s. 
+*/
+
 #include <stdio.h>
+
+void strcat(char *s, char *t);
 
 int main(void)
 {
+    char to[100] = "I love";
+    char what[] = " C";
 
+    strcat(to, what);
+
+    printf("%s", to);
 
     return 0;
 }
 
-void strcat(char s[], char t[])
+void strcat(char *s, char *t)
 {
-    int i, j;
-
-    i = j = 0;
-    while (s[i] != '\0')
-        i++;
-    while ((s[i++] = t[j++]) != '\0')
-        ;
+    while ( *s != '\0')
+        ++s;
+        
+     while ( *t != '\0')
+     {
+         *s = *t;
+         ++s;
+         ++t;
+     }
 }
+
+//thx to tehten
